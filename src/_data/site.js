79eb9f0805ef4c6a38e0af76da0df40f6464d006
Jeffrey Chiou <jeffchiou@gmail.com>
2020-11-23 12:05:56 -0500

    trying to add redirects

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,4 +1,4 @@
 module.exports = {
-  baseUrl: process.env.ELEVENTY_ENV === 'development' ? 'http://localhost:8080' : 'https://www.jeffchiou.com',
+  baseUrl: process.env.ELEVENTY_ENV === 'development' ? 'http://localhost:8080' : 'https://jeffchiou.com',
   title: "Jeff Chiou",
 };
